285566494121abe40ddbc70e6d66bf720aac841e
Adnan Hyder Volgo <adnan.volgo@gmail.com>
2020-06-04 18:48:50 +0400

    Updates

---
+++ precache-manifest.c8646dbb0c5dd6bcf1de0563fff563ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b990fbc59530b86dced69656afa4c90c",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "a1556222828fc4e7c51f",
+    "url": "/react-monster/static/css/main.75884b7f.chunk.css"
+  },
+  {
+    "revision": "299f65059b0504546074",
+    "url": "/react-monster/static/js/2.eca8dab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster/static/js/2.eca8dab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1556222828fc4e7c51f",
+    "url": "/react-monster/static/js/main.4167caae.chunk.js"
+  },
+  {
+    "revision": "eae9a61c0cb4937c185e",
+    "url": "/react-monster/static/js/runtime-main.b198fe31.js"
+  }
+]);
